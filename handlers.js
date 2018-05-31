565124f5d5df6fbb76434adba36d5a4dfcc8bc34
Jonas Enge <jonas.enge@gmail.com>
2018-05-31 16:50:55 +0200

    Genesis commit

--- handlers.js
+++ handlers.js
@@ -1,10 +1,21 @@
 const axios = require('axios')
-const getAuth = require('basic-auth')
+const bauth = require('basic-auth')
 const { readFile } = require('fs').promises
 const md = require('markdown-it')()
 const { send, json } = require('micro')
 const url = process.env.SVARINN_URL || 'https://svarut.ks.no/tjenester/svarinn'
 const filterString = process.env.FILTER_STRING || '(TFK-SIGN)'
+
+const sendError = (res, error = {}) => send(res, error.statusCode || 400, error.message)
+
+const getAuth = (req, res) => {
+  const auth = bauth(req)
+  if (!auth) return sendError(res, new Error('No auth'))
+  return {
+    username: auth.name,
+    password: auth.pass
+  }
+}
 
 exports.front = async (req, res) => {
   const readme = await readFile('./README.md', 'utf-8')
@@ -14,79 +25,67 @@ exports.front = async (req, res) => {
 
 exports.hentNyeForsendelser = async (req, res) => {
   const filter = req.params && req.params.filter
-  const auth = getAuth(req)
+  const auth = getAuth(req, res)
   const options = {
     url: `${url}/mottaker/hentNyeForsendelser`,
-    auth: {
-      username: auth.name,
-      password: auth.pass
-    }
+    auth
   }
   try {
     const { data } = await axios(options)
     const response = filter ? data.filter(item => item.tittel.includes(filterString)) : data.filter(item => !item.tittel.includes(filterString))
     send(res, 200, response)
   } catch (error) {
-    throw error
+    sendError(res, error)
   }
 }
 
 exports.hentForsendelsefil = async (req, res) => {
   const { id } = req.params
-  const auth = getAuth(req)
+  const auth = getAuth(req, res)
   const options = {
     url: `${url}/forsendelse/${id}`,
     // responseType:'stream',
-    auth: {
-      username: auth.name,
-      password: auth.pass
-    }
+    auth
   }
   try {
     const { data } = await axios(options)
     send(res, 200, data)
   } catch (error) {
-    throw error
+    sendError(res, error)
   }
 }
 
 exports.settForsendelseMottatt = async (req, res) => {
   const { id } = req.params
-  const auth = getAuth(req)
+  const auth = getAuth(req, res)
   const options = {
     url: `${url}/kvitterMottak/forsendelse/${id}`,
     method: 'post',
-    auth: {
-      username: auth.name,
-      password: auth.pass
-    }
+    auth
   }
 
   try {
     const { data } = await axios(options)
     send(res, 200, data)
   } catch (error) {
-    throw error
+    sendError(res, error)
   }
 }
 
 exports.settForsendelseMottakFeilet = async (req, res) => {
   const { id } = req.params
   const payload = await json(req)
-  const auth = getAuth(req)
+  const auth = getAuth(req, res)
   const options = {
     url: `${url}$/mottakFeilet/forsendelse/${id}`,
     method: 'post',
     data: payload,
-    auth: {
-      username: auth.name,
-      password: auth.pass
-    }
+    auth
   }
   try {
     const { data } = await axios(options)
     send(res, 200, data)
   } catch (error) {
-    throw error
+    sendError(res, error)
   }
 }
