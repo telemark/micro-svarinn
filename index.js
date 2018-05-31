6479a8f3d8cd90649fbf598e10dd14eb17157461
Jonas Enge <jonas.enge@gmail.com>
2018-05-31 15:11:20 +0200

    Genesis commit

--- index.js
+++ index.js
@@ -8,14 +8,11 @@ const handlers = require('./handlers')
 
 // Initialize a new router
 const router = Router()
-
-// Plugins
 router.use(cors())
 
 // Map routes to handlers
 router.get('/', handlers.front)
-router.get('/tjenester/svarinn/mottaker/hentNyeForsendelser', handlers.hentNyeForsendelser)
-router.get('/tjenester/svarinn/mottaker/hentNyeForsendelser/:filter', handlers.hentNyeForsendelser)
+router.get('/tjenester/svarinn/mottaker/hentNyeForsendelser/:filter?', handlers.hentNyeForsendelser)
 router.get('/tjenester/svarinn/forsendelse/:id', handlers.hentForsendelsefil)
 router.post('/tjenester/svarinn/kvitterMottak/forsendelse/:id', handlers.settForsendelseMottatt)
 router.post('/tjenester/svarinn/mottakFeilet/forsendelse/:id', handlers.settForsendelseMottakFeilet)
